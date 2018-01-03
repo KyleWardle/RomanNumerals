161abc4adbfd30aed38432dea463118ca9110e06
Unknown <kylewardle@bspokeit.co.uk>
2018-01-03 08:51:49 +0000

    Got login system working

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
         <div class="container">
           <h1 class="display-3" style="padding-top: 250px;">Roman Numerals Converter</h1>
           <p>This web application will convert base 10 numbers into Roman Numerals for you.</p>
-          <p style="padding-top: 30px;"><a class="login-button" href="#" role="button"><span>Sign in</span></a></p>
+          <p style="padding-top: 30px;"><a class="login-button" href="{{route('home')}}" role="button"><span>Sign in</span></a></p>
         </div>
       </div>
 
